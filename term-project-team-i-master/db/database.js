ba0cd399e2a5081fe8c40b6e0dd9e47f5923fdd3
Duy Nguyen <duynguyen@Duys-MacBook-Pro-2.local>
2022-04-24 06:59:16 -0700

    done by Softbrain

--- term-project-team-i-master/db/database.js
+++ term-project-team-i-master/db/database.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: "localhost",
     user: "root",
-    password: "happy11$",
+    password: "root@admin",
     database: "unodb",
     connectionLimit: 50,
     waitForConnection: true,
